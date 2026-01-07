b45ba83e58420cba0f691aff0c9d637f82a38824
max <merzlyakov029@gmail.com>
2026-01-07 06:45:41 +0500

    refactor: Thread  model: replace Optional[T] with T | None syntax

--- nightforo/types/thread/thread.py
+++ nightforo/types/thread/thread.py
@@ -1,4 +1,6 @@
-from typing import TYPE_CHECKING, Any, Dict, List, Optional
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Any
 
 from pydantic import BaseModel, Field
 
@@ -17,41 +19,41 @@ class Thread(BaseModel):
     title: str
     username: str
     user_id: int
-    user: Optional[User] = Field(alias="User", default=None)
-    is_watching: Optional[bool] = None
-    visitor_post_count: Optional[int] = None
-    is_unread: Optional[bool] = None
-    custom_fields: Optional[Dict[str, Any]] = None
-    tags: Optional[List[str]] = None
-    prefix: Optional[str] = None
-    can_edit: Optional[bool] = None
-    can_edit_tags: Optional[bool] = None
-    can_reply: Optional[bool] = None
-    can_soft_delete: Optional[bool] = None
-    can_hard_delete: Optional[bool] = None
-    can_view_attachments: Optional[bool] = None
-    view_url: Optional[str] = None
-    is_first_post_pinned: Optional[bool] = None
-    highlighted_post_ids: Optional[List[int]] = None
-    is_search_engine_indexable: Optional[bool] = None
-    index_state: Optional[str] = None
-    Forum: Optional[Node] = None
-    vote_score: Optional[int] = None
-    can_content_vote: Optional[bool] = None
-    allowed_content_vote_types: Optional[List[str]] = None
-    is_content_voted: Optional[bool] = None
-    visitor_content_vote: Optional[VoteTypeEnum] = None
-    reply_count: Optional[int] = None
-    view_count: Optional[int] = None
-    post_date: Optional[int] = None
-    sticky: Optional[bool] = None
-    discussion_state: Optional[str] = None
-    discussion_open: Optional[bool] = None
-    discussion_type: Optional[str] = None
-    first_post_id: Optional[int] = None
-    last_post_date: Optional[int] = None
-    last_post_id: Optional[int] = None
-    last_post_user_id: Optional[int] = None
-    last_post_username: Optional[str] = None
-    first_post_reaction_score: Optional[int] = None
-    prefix_id: Optional[int] = None
+    user: User | None = Field(alias="User", default=None)
+    is_watching: bool | None = None
+    visitor_post_count: int | None = None
+    is_unread: bool | None = None
+    custom_fields: dict[str, Any] | None = None
+    tags: list[str] | None = None
+    prefix: str | None = None
+    can_edit: bool | None = None
+    can_edit_tags: bool | None = None
+    can_reply: bool | None = None
+    can_soft_delete: bool | None = None
+    can_hard_delete: bool | None = None
+    can_view_attachments: bool | None = None
+    view_url: str | None = None
+    is_first_post_pinned: bool | None = None
+    highlighted_post_ids: list[int] | None = None
+    is_search_engine_indexable: bool | None = None
+    index_state: str | None = None
+    Forum: Node | None = None
+    vote_score: int | None = None
+    can_content_vote: bool | None = None
+    allowed_content_vote_types: list[str] | None = None
+    is_content_voted: bool | None = None
+    visitor_content_vote: VoteTypeEnum | None = None
+    reply_count: int | None = None
+    view_count: int | None = None
+    post_date: int | None = None
+    sticky: bool | None = None
+    discussion_state: str | None = None
+    discussion_open: bool | None = None
+    discussion_type: str | None = None
+    first_post_id: int | None = None
+    last_post_date: int | None = None
+    last_post_id: int | None = None
+    last_post_user_id: int | None = None
+    last_post_username: str | None = None
+    first_post_reaction_score: int | None = None
+    prefix_id: int | None = None
