63ec28706a0be7d91da059209e858e52b7980409
max <merzlyakov029@gmail.com>
2026-04-30 09:42:22 +0500

    chore: sync group names enum

--- nightforo/types/groups.py
+++ nightforo/types/groups.py
@@ -8,6 +8,7 @@ class ArzGuardGroupsIdsEnum(Enum):
     FORUM_ADMINISTRATORS = 25
     FORUM_MODERATORS = 36
     SERVICE_ACCOUNTS = 19
+    REPRESENTATIVE_ARIZONA_GAMES = 38
     GAME_ADMINISTRATORS = 17
     SERVER_MANAGERS = 33
     HEAD_MODERATORS = 4
@@ -17,6 +18,7 @@ class ArzGuardGroupsIdsEnum(Enum):
     SENIOR_MODERATORS = 7
     MODERATORS = 8
     BLOCKED_USERS = 5
+    USERS = 2
 
 
 class ArzGuardGroupsNamesEnum(Enum):
@@ -24,14 +26,15 @@ class ArzGuardGroupsNamesEnum(Enum):
     FORUM_ADMINISTRATORS = "(02) Администраторы форума"
     FORUM_MODERATORS = "(03) Модераторы форума"
     SERVICE_ACCOUNTS = "(04) Служебные аккаунты"
-    GAME_ADMINISTRATORS = "(05) Игровые администраторы"
-    SERVER_MANAGERS = "(06) Руководители серверов"
-    HEAD_MODERATORS = "(07) Главные модераторы"
-    DEPUTY_HEAD_MODERATORS = "(08) Заместители главных модераторов"
-    TECH_MODERATORS = "(09) Технические модераторы"
-    CURATORS_OF_MODERATION = "(10) Кураторы модерации"
-    SENIOR_MODERATORS = "(11) Старшие модераторы"
-    MODERATORS = "(12) Модераторы"
-    USERS = "(13) Пользователи"
-    GUESTS = "(14) Гости"
-    BLOCKED_USERS = "(15) Заблокированные пользователи"
+    REPRESENTATIVE_ARIZONA_GAMES = "(05) Представители Arizona Games"
+    GAME_ADMINISTRATORS = "(06) Игровые администраторы"
+    SERVER_MANAGERS = "(07) Руководители серверов"
+    TECH_MODERATORS = "(08) Технические модераторы"
+    HEAD_MODERATORS = "(09) Главные модераторы"
+    DEPUTY_HEAD_MODERATORS = "(10) Заместители главных модераторов"
+    CURATORS_OF_MODERATION = "(11) Кураторы модерации"
+    SENIOR_MODERATORS = "(12) Старшие модераторы"
+    MODERATORS = "(13) Модераторы"
+    USERS = "(14) Пользователи"
+    GUESTS = "(15) Гости"
+    BLOCKED_USERS = "(16) Заблокированные пользователи"
